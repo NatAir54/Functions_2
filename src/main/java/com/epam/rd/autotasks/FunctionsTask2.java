8673a12a79bc21d79daa357b8361e7c1981d7b73
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-07 20:01:22 +0300

    Fix FunctionsTask2.java

--- src/main/java/com/epam/rd/autotasks/FunctionsTask2.java
+++ src/main/java/com/epam/rd/autotasks/FunctionsTask2.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autotasks;
 
+import java.util.Objects;
+
 public class FunctionsTask2 {
     /**
      * <summary>
@@ -9,8 +11,32 @@ public class FunctionsTask2 {
      * IllegalArgumentException
      */
     public static boolean isSorted(int[] array, SortOrder order) {
-        //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        try {
+            if (array==null && order==null) {
+                throw new IllegalArgumentException();
+            }
+            if (array.length!=0 && order!=null) {
+                int j;
+                if (order==SortOrder.ASC) {
+                    for (j=0; j<array.length-1; j++) {
+                        if(array[j]>array[j+1])
+                            return false;
+                    }
+                    return true;
+                }
+                else if (order==SortOrder.DESC) {
+                    for (j=0; j<array.length-1; j++) {
+                        if(array[j]<array[j+1])
+                            return false;
+                    }
+                    return true;
+                }
+            }   else throw new IllegalArgumentException();
+        }
+        catch (NullPointerException e) {
+            return false;
+        }
+        return false;
     }
     /**
      * <summary>
@@ -20,7 +46,22 @@ public class FunctionsTask2 {
      * IllegalArgumentException
      */
     public static int[] transform(int[] array, SortOrder order) {
-       //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+
+            if (Objects.isNull(array)) {
+                throw new UnsupportedOperationException();
+            }
+            if (array.length == 0) {
+                throw new NullPointerException();
+            }
+            if (order != null && isSorted(array, order)) {
+                for (int i = 0; i < array.length; ++i) {
+                    array[i] = array[i] + i;
+                }
+            }
+
+
+            return array;
+
+
     }
 }
